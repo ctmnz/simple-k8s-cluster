d8530abd8c145d90b72c9afe5facabe4878dc01c
Daniel Stoinov <daniel.stoinov@gmail.com>
2026-02-04 02:14:00 +0200

    chore: fixed versions on few places

--- hack/run-dlq-consumer.sh
+++ hack/run-dlq-consumer.sh
@@ -1,2 +1,2 @@
-kubectl run kafka-consumer -ti --image=quay.io/strimzi/kafka:latest-kafka-3.7.0 --rm=true --restart=Never -- \
+kubectl run kafka-dlq-consumer -ti --image=quay.io/strimzi/kafka:latest-kafka-4.1.1 --rm=true --restart=Never -- \
   bin/kafka-console-consumer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic my-dlq-topic --from-beginning
--- hack/run-producer.sh
+++ hack/run-producer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl run kafka-producer -ti --image=quay.io/strimzi/kafka:latest-kafka-3.7.0 --rm=true --restart=Never -- \
+kubectl run kafka-producer -ti --image=quay.io/strimzi/kafka:latest-kafka-4.1.1 --rm=true --restart=Never -- \
   bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic my-topic
